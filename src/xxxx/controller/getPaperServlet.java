11baa3f568574c722384d509ea68ae25124562eb
dlyar <2022080902017@std.uestc.edu.cn>
2024-10-29 22:29:03 +0800

    update getPaperServlet.java

--- src/xxxx/controller/getPaperServlet.java
+++ src/xxxx/controller/getPaperServlet.java
@@ -16,15 +16,15 @@ import java.io.PrintWriter;
 @WebServlet("/getPaper")
 public class getPaperServlet extends HttpServlet {
 
-    private PaperGetService paperGetService = new PaperGetService();//调用服务层
+    private PaperGetService paperGetService = new PaperGetService();//实例化服务层
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 
-        MessageModel messageModel = paperGetService.paperGet();
+        MessageModel messageModel = paperGetService.paperGet();// 调用服务层的方法获取数据
 
-        resp.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
+        resp.setContentType("application/json;charset=UTF-8");// 设置响应内容类型为 JSON
+        PrintWriter out = resp.getWriter();// 获取响应输出流
 
         // 序列化为 JSON
         String json = new Gson().toJson(messageModel);

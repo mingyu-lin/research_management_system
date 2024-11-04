b463d891595bf4aaf3c97448f5265b22cb54ba20
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2024-11-04 21:13:47 +0800

    Add files via upload

--- src/xxxx/mapper/PaperGetMapper.java
+++ src/xxxx/mapper/PaperGetMapper.java
@@ -6,4 +6,7 @@ import java.util.List;
 
 public interface PaperGetMapper {
     List<Paper> getPaper();
+    List<Paper> getPaperByAuthor(String author);
+    List<Paper> getPaperByTitleAndAuthor_flagPos(String title,String author);
+    List<Paper> getPaper_flagNeg1();
 }

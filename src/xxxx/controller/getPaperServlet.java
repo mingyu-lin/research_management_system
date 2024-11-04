4ea1fe8ee9fd916c52aa2c3c0747ad172aa7aeac
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2024-11-04 21:12:49 +0800

    Add files via upload

--- src/xxxx/controller/getPaperServlet.java
+++ src/xxxx/controller/getPaperServlet.java
@@ -4,32 +4,56 @@ import com.google.gson.Gson;
 
 import xxxx.entity.value.MessageModel;
 import xxxx.service.PaperGetService;
+import xxxx.util.GetSqlSession;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Objects;
 
-@WebServlet("/getPapers")
+@WebServlet("/getPaper")
 public class getPaperServlet extends HttpServlet {
 
-    private PaperGetService paperGetService = new PaperGetService();//实例化服务层
+    private PaperGetService paperGetService = new PaperGetService();//调用服务层
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 
-        MessageModel messageModel = paperGetService.paperGet();// 调用服务层的方法获取数据
+        HttpSession session= req.getSession();
+        String role=(String)session.getAttribute("role");
 
-        resp.setContentType("application/json;charset=UTF-8");// 设置响应内容类型为 JSON
-        PrintWriter out = resp.getWriter();// 获取响应输出流
+        String title = req.getParameter("paperTitle");
+        String author = req.getParameter("paperAuthor");
+        int flag = Integer.parseInt(req.getParameter("paperFlag"));
+
+        MessageModel messageModel=new MessageModel();
+        if(Objects.equals(role, "user")|| ( Objects.equals(role,"admin")&&(flag!=1) ) ){
+            if(Objects.equals(title, "admin")){
+                messageModel =paperGetService.paperGetByAuthor(author);
+            }
+            else{
+                messageModel=paperGetService.paperGetByTitleAndAuthor_flagPos(title,author);
+            }
+        }
+        else if ( ( Objects.equals(role, "admin")&&(flag==1) ) ) {
+            messageModel=paperGetService.paperGet_flagNeg1();
+        }
+
+
+
+
+        resp.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
 
         // 序列化为 JSON
         String json = new Gson().toJson(messageModel);
         out.print(json);
-        out.flush();
+        out.flush();//确保更新
 
     }
 }

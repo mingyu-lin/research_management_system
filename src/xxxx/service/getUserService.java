a952788db3f96347ff4a9c08a0da6f0bc9cc6261
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2024-11-08 14:59:56 +0800

    Add files via upload

--- src/xxxx/service/getUserService.java
+++ src/xxxx/service/getUserService.java
@@ -2,10 +2,15 @@ package xxxx.service;
 
 import org.apache.ibatis.session.SqlSession;
 
+import xxxx.entity.News;
 import xxxx.entity.User;
 import xxxx.entity.value.MessageModel;
+import xxxx.mapper.NewsMapper;
 import xxxx.mapper.getUserMapper;
 import xxxx.util.GetSqlSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class getUserService {
 
@@ -28,4 +33,26 @@ public class getUserService {
         messageModel.setObject(info);
         return messageModel;
     }
+
+    public MessageModel getUserList() {
+        MessageModel messageModel = new MessageModel();
+        SqlSession session= GetSqlSession.createSqlSession();
+
+        getUserMapper mapper = session.getMapper(getUserMapper.class);//
+        List<User> info=mapper.getUserList();//调用mapper层获取新闻
+
+        messageModel.setCount(info.size());
+
+        if(info.size()==0){
+            User empty_info = new User();//
+            messageModel.setObject(empty_info);
+            messageModel.setCode(0);
+            messageModel.setMsg("无！");
+            return messageModel;
+        }
+        List<Object> objectList = new ArrayList<>(info);
+        messageModel.setList(objectList);
+        return messageModel;
+    }
+
 }

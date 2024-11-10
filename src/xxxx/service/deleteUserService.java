38145baea625a053abec98874df09a3495aa7a9c
mayanjiang <113437904+mayanjiang@users.noreply.github.com>
2024-11-10 21:06:01 +0800

    Add files via upload

---
+++ src/xxxx/service/deleteUserService.java
@@ -0,0 +1,19 @@
+package xxxx.service;
+
+import org.apache.ibatis.session.SqlSession;
+
+import xxxx.mapper.deleteUserMapper;
+import xxxx.mapper.getOnePaperMapper;
+import xxxx.util.GetSqlSession;
+
+public class deleteUserService {
+
+    public void deleteUser(int id) {
+        SqlSession session= GetSqlSession.createSqlSession();
+
+        deleteUserMapper mapper = session.getMapper(deleteUserMapper.class);//
+        mapper.deleteUser(id);//调用mapper层获取新闻
+
+    }
+
+}
